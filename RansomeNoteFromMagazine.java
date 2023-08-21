492083aab8425be999f496472733a052739ddbea
lizasu <jena.suchismita@gmail.com>
2023-08-20 22:28:49 -0400

    Update RansomeNoteFromMagazine.java

--- RansomeNoteFromMagazine.java
+++ RansomeNoteFromMagazine.java
@@ -1,4 +1,16 @@
 public boolean canConstruct(String ransomNote, String magazine) {
+        // better use 26 size array to keep track of count
+        // int[] arr = new int[26];
+        // for (int i = 0; i < magazine.length(); i++) {
+        //     arr[magazine.charAt(i) - 'a']++;
+        // }
+        // for (int i = 0; i < ransomNote.length(); i++) {
+        //     if(--arr[ransomNote.charAt(i)-'a'] < 0) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
         // create HashMap of all letters in magazine with count
         // for each character in ransomNote, reduce count from the hashmap if exists, otherwise return false
         HashMap<Character, Integer> dict = new HashMap<Character, Integer>();
