4463eda48e49e8edb61eef7c5a5513599385a096
lizasu <jena.suchismita@gmail.com>
2023-08-20 21:47:59 -0400

    Create QueueUsingStacks.java

---
+++ QueueUsingStacks.java
@@ -0,0 +1,27 @@
+// push into input stack
+// pop out of output stack; if empty, empty the input stack into output stack so the order is maintained
+class MyQueue {
+    // Use Two stacks
+    Stack<Integer> input = new Stack();
+    Stack<Integer> output = new Stack();
+    
+    public void push(int x) {
+        input.push(x);
+    }
+    
+    public int pop() {
+        peek();
+        return output.pop();
+    }
+    
+    public int peek() {
+        if (output.empty())
+            while (!input.empty())
+                output.push(input.pop());
+        return output.peek();
+    }
+    
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
